fb921b92c588f505036313bf0d88e86fe915c88b
HugoCidralDasChagas <hugochagasuk@gmail.com>
2023-03-16 18:49:04 +0000

    add test file, but not sure how to proceed. Waiting for Paul's reply

---
+++ src/test/java/qa_cafe/QACafe_test.java
@@ -0,0 +1,14 @@
+package qa_cafe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QACafe_test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
